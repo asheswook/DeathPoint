20e899925aca130913bb42b8d745d3df66a8ccad
Jaewook Lee <wookboy00@naver.com>
2023-11-30 01:08:24 +0900

    Chore: update package manager

--- src/main/java/ee/jwl/deathpoint/event/OnDeath.java
+++ src/main/java/ee/jwl/deathpoint/event/OnDeath.java
@@ -9,17 +9,31 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import ee.jwl.deathpoint.config.ConfigManager;
 import ee.jwl.deathpoint.DeathPoint;
 
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.OptionalInt;
+
 public class OnDeath implements Listener {
 
     private final FileConfiguration deathsConfig;
+    private final FileConfiguration config;
 
     public OnDeath(ConfigManager configManager) {
         this.deathsConfig = configManager.getConfig("deaths");
+        this.config = configManager.getConfig("config");
+    }
+
+    private String getFormattedKey(String playerUuid, int i){
+        return "deaths.%s.%d".formatted(playerUuid, i);
     }
 
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
-        byte maxSaves = 5;
+        Optional<String[]> targetWorlds = Optional.ofNullable((String[]) this.config.get("settings.targetWorlds"));
+        OptionalInt configMaxSaves = OptionalInt.of((int) this.config.get("settings.maxSaves"));
+        int maxSaves = configMaxSaves.orElse(5);
+
         Player player = event.getEntity();
         String playerUuid = player.getUniqueId().toString();
         Location playerPos = player.getLocation();
@@ -31,25 +45,27 @@ public class OnDeath implements Listener {
         boolean isDeathPointSaved = false;
         // 순회하며 null인 곳에 저장
         for (int i = 0; i < maxSaves; i++) {
-            String key = "deaths.%s.%d".formatted(playerUuid, i);
-            if (deathsConfig.get(key) == null) {
-                deathsConfig.set(key, formattedDeathPoint);
+            String key = getFormattedKey(playerUuid, i);
+            if (this.deathsConfig.get(key) == null) {
+                this.deathsConfig.set(key, formattedDeathPoint);
                 isDeathPointSaved = true;
+                break;
             }
         }
 
         // 저장할 곳이 없으면
         if (!isDeathPointSaved) {
-            // 최신 순으로 당기기
-            for (int i = maxSaves-1; 0 <= i; i--) {
-                String key = "deaths.%s.%d".formatted(playerUuid, i);
-                String prevKey = "deaths.%s.%d".formatted(playerUuid, i - 1);
-                deathsConfig.set(key, deathsConfig.get(prevKey));
+            // 최신 순으로 당기기, i == 0일때는 안함
+            for (int i = maxSaves - 1; 1 <= i; i--) {
+                String key = getFormattedKey(playerUuid, i);
+                String prevKey = getFormattedKey(playerUuid, i - 1);
+                Optional<String> prevDeathPoint = Optional.ofNullable(deathsConfig.get(prevKey).toString());
+                this.deathsConfig.set(key, prevDeathPoint.orElseThrow());
             }
 
             // 빈 0번에 저장
-            String key = "deaths.%s.%d".formatted(playerUuid, 0);
-            deathsConfig.set(key, formattedDeathPoint);
+            String key = getFormattedKey(playerUuid, 0);
+            this.deathsConfig.set(key, formattedDeathPoint);
         }
     }
 }
